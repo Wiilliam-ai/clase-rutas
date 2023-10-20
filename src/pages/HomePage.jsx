b4025e8413db1aa88294334ec301384d402af946
William Castro <wcastroh07@gmail.com>
2023-10-19 22:08:50 -0500

    new commit for linux

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage() {
   
   return (
     <div>
-        <h1>This is home</h1>
+        <h1>This is home in the linux add </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt culpa eos debitis, temporibus recusandae amet corporis! Veniam nulla delectus aperiam!</p>
     </div>
   )
